7b156fc5045d5030b2aaa9beff8d7833e02c62c3
Vasco Berardo <vasco.berardo@outlook.pt>
2026-03-27 09:33:21 -0400

    Add project files.

---
+++ ISYS366_VascoBerardo_Assignment3/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ISYS366_VascoBerardo_Assignment3.Data;
+using ISYS366_VascoBerardo_Assignment3.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ISYS366_VascoBerardo_Assignment3Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ISYS366_VascoBerardo_Assignment3Context") ?? throw new InvalidOperationException("Connection string 'ISYS366_VascoBerardo_Assignment3Context' not found.")));
+
+builder.Services.AddValidation();
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
