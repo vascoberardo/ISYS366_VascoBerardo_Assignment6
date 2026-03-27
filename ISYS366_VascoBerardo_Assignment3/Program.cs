7e188f9eda355e1bc8e1a350418d215718d3f533
Vasco Berardo <vasco.berardo@outlook.pt>
2026-03-27 12:24:38 -0400

    Admin account deletion from program.cs

--- ISYS366_VascoBerardo_Assignment3/Program.cs
+++ ISYS366_VascoBerardo_Assignment3/Program.cs
@@ -39,60 +39,6 @@ builder.Services.AddRazorPages(options =>
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-
-    SeedData.Initialize(services);
-}
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-using (var scope = app.Services.CreateScope())
-{
-    var roleManager =
-        scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-    // check if we already have an admin role
-    if (!await roleManager.RoleExistsAsync("Admin"))
-    {
-        // if not make the admin role
-        await roleManager.CreateAsync(new IdentityRole("Admin"));
-    }
-
-    // now we are going to make a default admin user
-    var userManager =
-        scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-
-    string email = "admin@mostuff.com";
-    // DANGER! PASSWORD MUST BE:
-    // 6+ chars
-    // at least one non alphanumerc character
-    // at least one digit ('0'-'9')
-    // at least one uppercase ('A'-'Z')
-    string password = "Password123!";
-
-    // see if we have already created the user
-    // if not create them and give them the admin role
-    if (await userManager.FindByEmailAsync(email) == null)
-    {
-        var user = new IdentityUser
-        {
-            UserName = email,
-            Email = email,
-            EmailConfirmed = true
-        };
-        await userManager.CreateAsync(user, password);
-        await userManager.AddToRoleAsync(user, "Admin");
-    }
-}
-
 app.UseHttpsRedirection();
 
 app.UseRouting();
